fa3e60a64ba344914f7c1d21946815cbb32f64b1
nape <nap3ntsoane@protonmail.com>
2025-01-29 16:02:00 +0200

    Tasks field added to event serializer

--- core/apps/event/serializers.py
+++ core/apps/event/serializers.py
@@ -7,87 +7,6 @@ from .models import (
 )
 
 
-class EventSerializer(serializers.ModelSerializer):
-	priority_display 					= serializers.CharField(source="get_priority_display", read_only=True)
-	event_type_display 					= serializers.CharField(source="get_event_type_display", read_only=True)
-	status_display 					= serializers.CharField(source="get_status_display", read_only=True)
-	bill_status_display 					= serializers.CharField(source="get_bill_status_display", read_only=True)
-
-	class Meta:
-		model = Event
-		fields = (
-			"id",
-			"title",
-			"code",
-			"details",
-			"priority",
-			"event_type",
-			"status",
-			"bill",
-			"bill_status",
-			"date_time",
-			"duration",
-
-			# Display
-			"priority_display",
-			"event_type_display",
-			"status_display",
-			"bill_status_display",
-
-			# Relational
-			"coordinator",
-			"client"
-		)
-		read_only_fields = (
-			"id",
-			"priority_display",
-			"event_type_display",
-			"status_display",
-			"bill_status_display",
-		)
-
-		depth = 1
-
-	def update(self, instance, validated_data):
-		instance = instance.update(**validated_data)
-		return instance	
-
-class EventSummarySerializer(serializers.ModelSerializer):
-	priority_display 					= serializers.CharField(source="get_priority_display", read_only=True)
-	event_type_display 					= serializers.CharField(source="get_event_type_display", read_only=True)
-	status_display 					= serializers.CharField(source="get_status_display", read_only=True)
-	bill_status_display 					= serializers.CharField(source="get_bill_status_display", read_only=True)
-
-	class Meta:
-		model = Event
-		fields = (
-			"id",
-			"title",
-			"code",
-			"date_time",
-			"duration",
-
-			# Display
-			"priority_display",
-			"event_type_display",
-			"status_display",
-			"bill_status_display",
-
-			# Relational
-			"client"
-		)
-		read_only_fields = (
-			"id",
-			"priority_display",
-			"event_type_display",
-			"status_display",
-			"bill_status_display",
-		)
-
-		depth = 1
-
-
-
 class TaskSerializer(serializers.ModelSerializer):
 	priority_display 					= serializers.CharField(source="get_priority_display", read_only=True)
 	status_display 					= serializers.CharField(source="get_status_display", read_only=True)
@@ -142,5 +61,104 @@ class TaskSummarySerializer(serializers.ModelSerializer):
 			"priority_display",
 			"status_display",
 		)
+
+
+class TaskSerializerField(serializers.RelatedField):
+	def to_representation(self, value):
+		serialized_tasks = []
+		for task in value.all()[:5]:
+			serialized_tasks.append(TaskSummarySerializer(task).data)
+
+		return serialized_tasks
+				
+
+class EventSerializer(serializers.ModelSerializer):
+	priority_display 					= serializers.CharField(source="get_priority_display", read_only=True)
+	event_type_display 					= serializers.CharField(source="get_event_type_display", read_only=True)
+	status_display 						= serializers.CharField(source="get_status_display", read_only=True)
+	bill_status_display 				= serializers.CharField(source="get_bill_status_display", read_only=True)
+
+	tasks 								= TaskSerializer(read_only=True, many=True, source="get_tasks")
+
+	class Meta:
+		model = Event
+		fields = (
+			"id",
+			"title",
+			"code",
+			"details",
+			"priority",
+			"event_type",
+			"status",
+			"bill",
+			"bill_status",
+			"date_time",
+			"duration",
+
+			"tasks",
+
+			# Display
+			"priority_display",
+			"event_type_display",
+			"status_display",
+			"bill_status_display",
+
+			# Relational
+			"coordinator",
+			"client"
+		)
+		read_only_fields = (
+			"id",
+			"priority_display",
+			"event_type_display",
+			"status_display",
+			"bill_status_display",
+		)
+
+		depth = 1
+
+	def update(self, instance, validated_data):
+		instance = instance.update(**validated_data)
+		return instance	
+
+class EventSummarySerializer(serializers.ModelSerializer):
+	priority_display 					= serializers.CharField(source="get_priority_display", read_only=True)
+	event_type_display 					= serializers.CharField(source="get_event_type_display", read_only=True)
+	status_display 					= serializers.CharField(source="get_status_display", read_only=True)
+	bill_status_display 					= serializers.CharField(source="get_bill_status_display", read_only=True)
+
+	tasks 								=  TaskSerializerField(read_only=True)
+
+
+	class Meta:
+		model = Event
+		fields = (
+			"id",
+			"title",
+			"code",
+			"date_time",
+			"duration",
+
+			# Display
+			"priority_display",
+			"event_type_display",
+			"status_display",
+			"bill_status_display",
+
+			"tasks",
+
+			# Relational
+			"client"
+		)
+		read_only_fields = (
+			"id",
+			"priority_display",
+			"event_type_display",
+			"status_display",
+			"bill_status_display",
+		)
+
+		depth = 1
+
 
 
